91760692c615db1dc3a3433891aaabb333e71aae
Kofi Ladd-Oliver <02kofi.laddoliver@gulllakecs.org>
2020-02-03 12:09:33 -0800

    Intro Multi line comment

--- Arduino_PC_Temp_Project/Arduino_PC_Temp_Project.ino
+++ Arduino_PC_Temp_Project/Arduino_PC_Temp_Project.ino
@@ -1,3 +1,11 @@
+/*
+ * Programmer: Kofi Ladd-Oliver
+ * Program: CPU Temp
+ * Date: 2.3.20
+ * 
+ * This program will show me the tempurature of my CPU
+ */
+
 void setup() {
   // put your setup code here, to run once:
 
